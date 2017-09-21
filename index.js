1daf27ea430e0f0c7aee98cb7b17b17afa4734b5
Mr.Zhang <china_mobile2008@sina.com>
2017-09-21 08:42:44 +0800

    update

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ function wrapRegisterApp(nativeFunc) {
   }
   return (...args) => {
     if (isAppRegistered) {
-      // FIXME(Yorkie): we ignore this error if AppRegistered is true.
       return Promise.resolve(true);
     }
     isAppRegistered = true;
@@ -33,11 +32,109 @@ function wrapRegisterApp(nativeFunc) {
   };
 }
 
+function wrapApi(nativeFunc) {
+  if (!nativeFunc) {
+    return undefined;
+  }
+  return (...args) => {
+    if (!isAppRegistered) {
+      return Promise.reject(new Error('registerApp required.'));
+    }
+    return new Promise((resolve, reject) => {
+      nativeFunc.apply(null, [
+        ...args,
+        (error, result) => {
+          if (!error) {
+            return resolve(result);
+          }
+          if (typeof error === 'string') {
+            return reject(new Error(error));
+          }
+          reject(error);
+        },
+      ]);
+    });
+  };
+}
+
 /**
- * @method registerApp
- * @param {String} appid - the app id
- * @param {String} secret - the secret
- * @return {Promise}
+ *  注册app
+ *
+ *  @param appId  开发者网站申请的appid
+ *  @param secret 开发者网站申请的secret
  */
 export const registerApp = wrapRegisterApp(DIDI.registerApp);
 
+/**
+ *  通过该方法调起滴滴页面
+ *
+ *  @param parentController 展现滴滴页面的ViewController
+ *  @param animated         展现滴滴页面时是否需要动画
+ *  @param optionParams     可选参数
+ *  @param delegate         代理
+ */
+export const showDDPage = wrapRegisterApp(DIDI.showDDPage);
+
+/**
+*  拨打手机号码
+*
+*  @param phone  手机号码加密字符串
+*  @param prompt 拨打电话是否弹出提示
+*  @param error  错误信息
+*/
+export const callPhone = wrapRegisterApp(DIDI.callPhone);
+
+/**
+*  异步获取ticket
+*
+*  @param ticketType  ticketType分为single和long两种，single类型每次请求完就失效，long类型有一定失效周期。
+*  @param resultBlock 结果回调
+*/
+export const asyncGetTicket = wrapRegisterApp(DIDI.asyncGetTicket);
+
+/**
+*  同步获取ticket
+*
+*  @param ticketType  ticketType分为single和long两种，single类型每次请求完就失效，long类型有一定失效周期。
+*  @param resultError 错误信息
+*
+*  @return 返回结果数据
+*/
+export const syncGetTicket = wrapRegisterApp(DIDI.syncGetTicket);
+
+/**
+*  打开制定页面
+*
+*  @param pageName    页面名称
+*  @param params      所需参数信息
+*  @param navTheme    页面导航栏定制
+*  @param resultBlock 结果回调,回调中返回页面的viewController
+*/
+export const openPage = wrapRegisterApp(DIDI.openPage);
+
+/**
+*  异步请求开放API接口
+*
+*  @param apiName     API名称
+*  @param dict        API所需参数
+*  @param resultBlock 结果回调
+*/
+export const asyncCallOpenAPI = wrapRegisterApp(DIDI.asyncCallOpenAPI);
+
+/**
+*  同步请求开放API接口
+*
+*  @param apiName     API名称
+*  @param dict        API所需参数
+*  @param resultError 错误信息
+*
+*  @return 返回结果数据
+*/
+export const syncCallOpenAPI = wrapRegisterApp(DIDI.syncCallOpenAPI);
+
+/**
+*  检查打车主页是否登陆
+*
+*  @return 已登录返回YES,未登录返回NO
+*/
+export const checkLogin = wrapRegisterApp(DIDI.checkLogin);
--- ios/RCTDIDI/RCTDIDI.m
+++ ios/RCTDIDI/RCTDIDI.m
@@ -2,7 +2,7 @@
 //  RCTDIDI.m
 //  RCTDIDI
 //
-//  Created by zhangkun on 2017/9/20.
+//  Created by china-mobile2008 on 2017/9/20.
 //  Copyright © 2017年 china-mobile2008. All rights reserved.
 //
 
@@ -20,7 +20,7 @@
 
 @synthesize bridge = _bridge;
 
-RCT_EXPORT_MODULE()
+RCT_EXPORT_MODULE();
 
 RCT_EXPORT_METHOD(registerApp:(NSString *)appid
                   :(NSString*)secret
@@ -28,7 +28,81 @@ RCT_EXPORT_METHOD(registerApp:(NSString *)appid
 {
     self.appId = appid;
     self.secret = secret;
-    callback(@[[NSNull null], @([DIOpenSDK registerApp:appid secret:secret])]);
+    callback(@[[DIOpenSDK registerApp:appid secret:secret]]);
 }
 
+RCT_EXPORT_METHOD(showDDPage:(UIViewController *)parentController
+                  :(BOOL)animated
+                  :(DIOpenSDKRegisterOptions *)optionParams
+                  :(id<DIOpenSDKDelegate>)delegate
+                  :(RCTResponseSenderBlock)callback)
+{
+    callback(@[[DIOpenSDK showDDPage:parentController
+                            animated:animated
+                              params:optionParams
+                            delegate:delegate]]);
+}
+
+RCT_EXPORT_METHOD(callPhone:(NSString *)phone
+                  :(BOOL)prompt
+                  :(NSError *__autoreleasing *)error
+                  :(RCTResponseSenderBlock)callback)
+{
+    callback(@[[DIOpenSDK callPhone:phone
+                             prompt:prompt
+                              error:error]]);
+}
+
+RCT_EXPORT_METHOD(asyncGetTicket:(NSString *)ticketType
+                  :(void (^)(NSError *error,DIBaseModel *model))resultBlock
+                  :(RCTResponseSenderBlock)callback)
+{
+    callback(@[[DIOpenSDK asyncGetTicket:ticketType
+                             resultBlock:resultBlock]]);
+}
+
+RCT_EXPORT_METHOD(syncGetTicket:(NSString *)ticketType
+                  :(NSError *__autoreleasing *)resultError
+                  :(RCTResponseSenderBlock)callback)
+{
+    callback(@[[DIOpenSDK syncGetTicket:ticketType
+                            resultError:resultError]]);
+}
+
+RCT_EXPORT_METHOD(openPage:(NSString *)pageName
+                  :(NSDictionary *)params
+                  :(DITopNavigationTheme *)navTheme
+                  :(void (^)(NSError *error,UIViewController *viewController))resultBlock
+                  :(RCTResponseSenderBlock)callback)
+{
+    callback(@[[DIOpenSDK openPage:pageName
+                            params:params
+                          navTheme:navTheme
+                       resultBlock:resultBlock]]);
+}
+
+RCT_EXPORT_METHOD(asyncCallOpenAPI:(NSString *)apiName
+                  :(NSDictionary *)dict
+                  :(void (^)(NSError *error,DIBaseModel *model))resultBlock
+                  :(RCTResponseSenderBlock)callback)
+{
+    callback(@[[DIOpenSDK asyncCallOpenAPI:apiName
+                                    params:dict
+                               resultBlock:resultBlock]]);
+}
+
+RCT_EXPORT_METHOD(syncCallOpenAPI:(NSString *)apiName
+                  :(NSDictionary *)dict
+                  :(NSError *__autoreleasing *)resultError
+                  :(RCTResponseSenderBlock)callback)
+{
+    callback(@[[DIOpenSDK syncCallOpenAPI:apiName 
+                                   params:dict
+                              resultError:resultError]]);
+}
+
+RCT_EXPORT_METHOD(checkLogin :(RCTResponseSenderBlock)callback)
+{
+    callback(@[[DIOpenSDK checkLogin]]);
+}
 @end
